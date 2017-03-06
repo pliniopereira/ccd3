ac5ff723c639b71c5b4906d9f9ffce67173b60a5
Plínio Pereira <pliniojr@gmail.com>
2017-03-06 19:53:54 -0300

    Add FilterWheel_Control

--- src/Main.py
+++ src/Main.py
@@ -98,11 +98,68 @@ def home_reset():
 
 home_reset()
 
+def FilterWheel_Control(FilterNumber):
+    if FilterNumber == 1:
+        command = "g=1"
+    if FilterNumber == 2:
+        command = "g=2"
+    if FilterNumber == 3:
+        command = "g=3"
+    if FilterNumber == 4:
+        command = "g=4"
+    if FilterNumber == 5:
+        command = "g=5"
+    if FilterNumber == 6:
+        command = "g=6"
 
-        #['AddNetMotor', 'AddRef', 'AddressMotorChain', 'AddressServos', 'BaudRate', 'BytesAvailable', 'CharDelay', 'ClearBuffer',
-#  'ClearEEPROM', 'ClosePort', 'DefaultMotor', 'DetectNetMotors', 'DetectRS232', 'DetectRS485', 'DetectUSBMotors',
-#  'Download', 'Echo', 'EchoTimeoutConst', 'EchoTimeoutMul', 'EngineVersion', 'EstablishChain', 'ForceUpload',
-#  'GetIDsOfNames', 'GetMotor','GetResponseOf', 'GetTypeInfo', 'GetTypeInfoCount', 'InitEthernet', 'InitRS485', 'InitUSB',
-#  'InitializeNotification', 'Invoke', 'IsRS485', 'LogFileName', 'LogFlags', 'MaxMotors', 'NoOfMotors', 'OpenPort',
-#  'Parity', 'PortHandle', 'PortName', 'QueryInterface', 'ReadResponse', 'ReadString', 'Release', 'ReorderMotors',
-#  'Timeout', 'TxMaxRetry', 'TxTimeoutConst', 'TxTimeoutMul', 'Upload', 'Wait', 'WriteCmd', 'WriteCommand', 'WriteString',
+    CommInterface.WriteCommand(command)       #Send filter position
+    hPosition = 1
+    g = FilterNumber                            #Filter position
+    h = hPosition                               #Present position
+
+    if h == 1: #Present position is 3333
+        if g < 5:
+            CommInterface.WriteCommand("P=g*3333")
+        if g == 5: #Move in opposite direction
+            CommInterface.WriteCommand("P=-3333")
+        if g == 6: #Move in opposite direction
+            CommInterface.WriteCommand("P=0")
+
+    if h == 2: #Present position is 6666
+        if g < 6:
+            CommInterface.WriteCommand("P=g*3333")
+        if g == 6: #Move in opposite direction
+            CommInterface.WriteCommand("P=0")
+
+    if h == 3: #Present position is 9999
+        CommInterface.WriteCommand("P=g*3333")
+
+    if h == 4: #Present position is 13332
+        CommInterface.WriteCommand("P=g*3333")
+
+    if h == 5: #Present position is 16665
+        if g == 1:
+            CommInterface.WriteCommand("P=23333")
+        if g == 2:
+            CommInterface.WriteCommand("P=-26666")
+        if g > 2:
+            CommInterface.WriteCommand("P=g*3333")
+
+    if h == 6: #Present position is 20000
+        if g == 1:
+            CommInterface.WriteCommand("P=23333")
+        if g == 2:
+            CommInterface.WriteCommand("P=26666")
+        if g > 2:
+            CommInterface.WriteCommand("P=g*3333")
+
+    CommInterface.WriteCommand("G") #Make the filter move to next position
+    sleep(1.5) #Wait until the trajectory is finished
+
+    CommInterface.WriteCommand("h=g") #Reset the present filter position
+    CommInterface.WriteCommand("O=h*3333") #And reset the present origin
+    CommInterface.WriteCommand("END")
+    hPosition = FilterNumber #h receive g for VB use
+
+
+FilterWheel_Control(1)
