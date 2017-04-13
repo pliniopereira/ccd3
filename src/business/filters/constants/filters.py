66f38cd125e54f7480d33cc78207fdb1a243f150
Plínio Pereira <pliniojr@gmail.com>
2017-04-13 18:59:05 -0300

    Create constants filters

--- src/business/filters/constants/filters.py
+++ src/business/filters/constants/filters.py
@@ -1,3 +1,36 @@
 FILTERSFILE = "filters"
 
-X = 'x'
+LABEL_FILTER1 = "label_filter1"
+LABEL_FILTER2 = "label_filter2"
+LABEL_FILTER3 = "label_filter3"
+LABEL_FILTER4 = "label_filter4"
+LABEL_FILTER5 = "label_filter5"
+LABEL_FILTER6 = "label_filter6"
+
+WAVELENGTH_FILTER1 = "wavelength_filter1"
+WAVELENGTH_FILTER2 = "wavelength_filter2"
+WAVELENGTH_FILTER3 = "wavelength_filter3"
+WAVELENGTH_FILTER4 = "wavelength_filter4"
+WAVELENGTH_FILTER5 = "wavelength_filter5"
+WAVELENGTH_FILTER6 = "wavelength_filter6"
+
+EXPOSURE_FILTER1 = "exposure_filter1"
+EXPOSURE_FILTER2 = "exposure_filter2"
+EXPOSURE_FILTER3 = "exposure_filter3"
+EXPOSURE_FILTER4 = "exposure_filter4"
+EXPOSURE_FILTER5 = "exposure_filter5"
+EXPOSURE_FILTER6 = "exposure_filter6"
+
+BINNING_FILTER1 = "binning_filter1"
+BINNING_FILTER2 = "binning_filter2"
+BINNING_FILTER3 = "binning_filter3"
+BINNING_FILTER4 = "binning_filter4"
+BINNING_FILTER5 = "binning_filter5"
+BINNING_FILTER6 = "binning_filter6"
+
+CCDGAIN_FILTER1 = "ccdgain_filter1"
+CCDGAIN_FILTER2 = "ccdgain_filter2"
+CCDGAIN_FILTER3 = "ccdgain_filter3"
+CCDGAIN_FILTER4 = "ccdgain_filter4"
+CCDGAIN_FILTER5 = "ccdgain_filter5"
+CCDGAIN_FILTER6 = "ccdgain_filter6"
