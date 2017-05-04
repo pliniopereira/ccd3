80ea6208aee976530875a7cba300f6d36432a4fd
Plínio Pereira <pliniojr@gmail.com>
2017-05-04 18:43:35 -0300

    Update filter_control

--- src/utils/rodafiltros/Filter_control.py
+++ src/utils/rodafiltros/Filter_control.py
@@ -7,10 +7,10 @@ from PyQt5 import QtCore
 from src.utils.rodafiltros import Leitura_portas
 
 
-class FILTER_CLASS(QtCore.QThread):
+class Filter_control(QtCore.QThread):
 
     def __init__(self, parent=None):
-        super(FILTER_CLASS, self).__init__(parent)
+        super(Filter_control, self).__init__(parent)
         self.smi = cc.CreateObject('SMIEngine.SMIHost')
         cc.GetModule('IntegMotorInterface.dll')
         self.CommInterface = self.smi.QueryInterface(comtypes.gen.INTEGMOTORINTERFACELib.ISMIComm)
