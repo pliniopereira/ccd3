5901a26981fe1978e61ffba2045426e22b228dd0
Plínio Pereira <pliniojr@gmail.com>
2017-05-04 18:25:18 -0300

    Filter_control class :tada:

--- src/utils/rodafiltros/Main.py
+++
@@ -1,9 +0,0 @@
-from src import CommInterface
-from src.ControladoresFiltros import Shutter
-
-CommInterfaceVar, Motor = CommInterface.create_object()
-print(dir(CommInterface))
-
-CommInterface.Shutter.open_shutter()
-
-#Shutter.open_shutter(CommInterfaceVar)
